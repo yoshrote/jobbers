56f38517548ccef4f6d684aa4bdf1bd200f806dc
Josh Forman <josh@yoshrote.com>
2025-04-04 15:31:39 -0400

    ignore dummy tasks in the coverage report

--- tests/test_registry.py
+++ tests/test_registry.py
@@ -3,6 +3,7 @@ import pytest
 from jobbers.models import TaskConfig
 from jobbers.registry import get_task_config, register_task
 
+
 @pytest.fixture(autouse=True)
 def setup():
     """Fixture to reset the tasks in the mocked Redis before each test."""
@@ -10,12 +11,10 @@ def setup():
     from jobbers.registry import _task_function_map
     _task_function_map.clear()
 
-    yield
-
 def test_register_task_success():
     """Test successful registration of a task function."""
     @register_task(name="test_task", version=1)
-    def test_function():
+    def test_function():  # pragma: no cover
         pass
 
     task_config = get_task_config("test_task", 1)
@@ -28,7 +27,7 @@ def test_register_task_re_registration():
     """Test re-registration of the same function for the same name and version."""
     @register_task(name="test_task", version=1)
     @register_task(name="test_task", version=1)
-    def test_function():
+    def test_function(): # pragma: no cover
         pass
 
     task_config = get_task_config("test_task", 1)
@@ -38,12 +37,12 @@ def test_register_task_re_registration():
 def test_register_task_different_function_same_name_version():
     """Test registering a different function with the same name and version raises an exception."""
     @register_task(name="test_task", version=1)
-    def test_function_1():
+    def test_function_1(): # pragma: no cover
         pass
 
     with pytest.raises(ValueError, match="Task test_task version 1 is already registered to another function"):
         @register_task(name="test_task", version=1)
-        def test_function_2():
+        def test_function_2(): # pragma: no cover
             pass
 
 def test_register_task_non_callable():
@@ -54,7 +53,7 @@ def test_register_task_non_callable():
 def test_get_task_config_found():
     """Test retrieving a registered task configuration."""
     @register_task(name="test_task", version=1)
-    def test_function():
+    def test_function(): # pragma: no cover
         pass
 
     task_config = get_task_config("test_task", 1)
