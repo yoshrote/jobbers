f80ca4a9097c82c76767be3ba643e17d0f39d6ac
Josh Forman <josh@yoshrote.com>
2026-05-06 17:17:15 -0400

    consts
    closes #68

--- jobbers/utils/serialization.py
+++ jobbers/utils/serialization.py
@@ -8,23 +8,26 @@ EMPTY_DICT = b"\x80"
 EMPTY_LIST = b"\x90"
 NONE = b"\xc0"
 
+EXT_DATETIME = 1
+EXT_TIMEDELTA = 2
+EXT_ULID = 3
 
 def default(obj: object) -> Any:
     if isinstance(obj, datetime.datetime):
-        return msgpack.ExtType(1, obj.isoformat().encode())
+        return msgpack.ExtType(EXT_DATETIME, obj.isoformat().encode())
     elif isinstance(obj, datetime.timedelta):
-        return msgpack.ExtType(2, str(obj.total_seconds()).encode())
+        return msgpack.ExtType(EXT_TIMEDELTA, str(obj.total_seconds()).encode())
     elif isinstance(obj, ULID):
-        return msgpack.ExtType(3, bytes(obj))
+        return msgpack.ExtType(EXT_ULID, bytes(obj))
     raise TypeError(f"Unknown type: {obj!r}")
 
 
 def ext_hook(code: int, data: bytes) -> Any:
-    if code == 1:
+    if code == EXT_DATETIME:
         return datetime.datetime.fromisoformat(data.decode())
-    elif code == 2:
+    elif code == EXT_TIMEDELTA:
         return datetime.timedelta(seconds=float(data.decode()))
-    elif code == 3:
+    elif code == EXT_ULID:
         return ULID.from_bytes(data)
     return msgpack.ExtType(code, data)
 
